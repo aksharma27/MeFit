7b827f3ce9a7d1948fdab434fab819ff64f6a182
abhishek <aksharma.27mjm@gmail.com>
2024-07-29 23:34:58 +0530

    logo changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import Logo from '../assets/images/Logo.png';
 const Navbar = () => (
   <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
     <Link to="/">
-      <img src={Logo} alt="logo" style={{ width: '70px', height: '100x', margin: '0px 20px' }} />
+      <img src="https://avatars.githubusercontent.com/u/83135634?v=4" alt="logo" style={{ width: '70px', height: '100x', margin: '0px 20px' }} />
     </Link>
     <Stack
       direction="row"
